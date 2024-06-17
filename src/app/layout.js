87554ab0ed2831fe43d51888681293097b009563
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-06-17 12:31:51 +0700

    add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import "./global.css";
 import { CardContextProvider } from "@/context/CardContext";
+export const metadata = {
+  title: "Simple English Flash Card",
+  description: "Next.js application for creating simple english flash card",
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="cupcake">
